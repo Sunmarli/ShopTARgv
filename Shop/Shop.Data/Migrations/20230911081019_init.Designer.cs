45d7c1dd0900b851d359bd58a043e0ad1634e835
Maria Jarv <maria.julia.jarv@gmail.com>
2023-09-11 11:13:21 +0300

    database creation and connection

--- Shop/Shop.Data/Migrations/20230911081019_init.Designer.cs
+++ Shop/Shop.Data/Migrations/20230911081019_init.Designer.cs
@@ -12,7 +12,7 @@ using Shop.Data;
 namespace Shop.Data.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    [Migration("20230911073038_init")]
+    [Migration("20230911081019_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ShopContext>(Options =>Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ShopContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
