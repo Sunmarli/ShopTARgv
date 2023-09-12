432d5f832221f649c80bde4a59c22b0b3dc60f1d
Opilane TTHK <opilane@tthk.ee>
2023-09-12 14:43:59 +0300

    dto file creation

---
+++ Shop/Shop.Core/Dto/SpaceshipDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Dto
+{
+    public class SpaceshipDto
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Passengers { get; set; }
+        public int EnginePower { get; set; }
+        public int Crew { get; set; }
+        public string Company { get; set; }
+        public int CargoWeight { get; set; }
+
+        //only in database
+        public DateTime CreatedAt { get; set; }
+        public DateTime Modified { get; set; }
+
+
+    }
+}
--- Shop/Shop/Controllers/SpaceshipsController.cs
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Dto;
 using Shop.Data;
 using Shop.Models.Spaceship;
 using System.Xml.Linq;
@@ -6,25 +7,25 @@ using System.Xml.Linq;
 namespace Shop.Controllers
 {
     public class SpaceshipsController : Controller
-    { 
+    {
         private readonly ShopContext _context;
 
         public SpaceshipsController(ShopContext context)
         { this._context = context; }
 
-    
+
         public IActionResult Index()
-        
+
         {
             var result = _context.Spaceships
             .Select(x => new SpaceshipsIndexViewModel()
             {
-                Id=x.Id,
+                Id = x.Id,
                 Name = x.Name,
                 Type = x.Type,
                 EnginePower = x.EnginePower,
                 Passengers = x.Passengers
-             
+
             });
 
             return View(result);
@@ -33,6 +34,24 @@ namespace Shop.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SpaceshipsCreateViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                Type = vm.Type,
+                Passengers = vm.Passengers,
+                EnginePower = vm.EnginePower,
+                Company = vm.Company,
+                Crew = vm.Crew,
+                CargoWeight = vm.CargoWeight,
+
+            };
         }
     }
 }
